fa127a087196ac11b1a098d4f81cb26794012f16
alana anjos <anjosa840@gmail.com>
2026-02-14 23:55:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let herois = [
+    { nome: "Arthas", xp: 500 },
+    { nome: "Jaina", xp: 3500 },
+    { nome: "Thrall", xp: 12000 }
+];
+
+for (let i = 0; i < herois.length; i++) {
+    let nivel = "";
+    let xp = herois[i].xp;
+
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+
+    console.log(`O Herói de nome ${herois[i].nome} está no nível de ${nivel}`);
+}
